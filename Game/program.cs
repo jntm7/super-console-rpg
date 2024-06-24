23bd1642fc1a9943cbd386136effb26f4732beeb
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-24 13:29:21 -0600

    Create program.cs

---
+++ Game/program.cs
@@ -0,0 +1,11 @@
+namespace SuperConsoleRPG.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEngine engine = new GameEngine();
+            engine.Run();
+        }
+    }
+}
