b932adf9b962a8dd381a3c8cc3e6d882ec022ef8
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-24 17:15:14 -0600

    Update item.cs

--- engine/entities/item.cs
+++ engine/entities/item.cs
@@ -1,25 +1,21 @@
 namespace SuperConsoleRPG.Engine.Entities
 {
-    public enum ItemEffect
-    {
-        Heal,
-        AttackBuff,
-        DefenseBuff
-    }
-
     public class Item
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public ItemEffect Effect { get; set; }
-        public int EffectValue { get; set; }
 
-        public Item(string name, string description, ItemEffect effect, int effectValue)
+        public Item(string name, string description, ItemEffect effect)
         {
             Name = name;
             Description = description;
             Effect = effect;
-            EffectValue = effectValue;
+        }
+
+        public void Use(Character character)
+        {
+            Effect.Apply(character);
         }
     }
 }
